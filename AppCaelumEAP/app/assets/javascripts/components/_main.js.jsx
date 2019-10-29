7449b82150b0f3031d2d782847c15b6f62a472b4
gstadtler <ghs2@cin.ufpe.br>
2019-10-28 23:27:46 -0300

    criação de telas de cadastro e login com React

---
+++ AppCaelumEAP/app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,29 @@
+const Main = (props) => {
+  return(
+    <div>
+      <h1>Welcome to CaelumEAP!</h1>
+    </div>
+  )
+}
+const Login = (props) => {
+  return(
+    <div>
+      <LoginPage />
+    </div>
+  )
+}
+const Cadastro = (props) => {
+  return(
+    <div>
+      <UserCadastro />
+    </div>
+  )
+}
+const Paciente = (props) => {
+  return(
+    <div>
+      <h1>Meus Pacientes</h1>
+      <Pacientes />
+    </div>
+  )
+}
